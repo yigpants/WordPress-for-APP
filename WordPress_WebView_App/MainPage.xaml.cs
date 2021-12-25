a8d90631f9d27eb3d8b4206303e10de27693fe9d
yigpants <s980957582@gmail.com>
2021-12-25 22:51:25 +0800

    C#共用程式

---
+++ WordPress_WebView_App/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WordPress_WebView_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoBackButton_Clicked(Object sender, EventArgs e)
+        {
+            if (mainWebView.CanGoBack)
+            {
+                mainWebView.GoBack();
+            }
+        }
+
+        private void GoForwardButton_Clicked(Object sender, EventArgs e)
+        {
+            if (mainWebView.CanGoForward)
+            {
+                mainWebView.GoForward();
+            }
+        }
+        private void ReloadButton_Clicked(Object sender, EventArgs e)
+        {
+            mainWebView.Reload();
+        }
+
+        private void mainWebView_Navigated(Object sender, WebNavigatedEventArgs e)
+        {
+            displayLabel.Text = (mainWebView.Source as UrlWebViewSource).Url;
+        }
+    }
+}
